02cafe18c08d2d4dc6025d95b4c1cf9b12b60b8e
Romulo Messias <romulomessias@me.com>
2019-09-19 20:36:22 -0300

    added mobx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import {configure} from 'mobx';
+
+configure({ enforceActions: 'observed' });
 
 const App: React.FC = () => <section>Hello World!</section>;
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './pages/App';
+import App from './App';
 import { register } from './ServiceWorker';
 
 ReactDOM.render(

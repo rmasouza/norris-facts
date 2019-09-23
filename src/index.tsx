8229961065b6807f2db61a18bf16f330d42a9bbc
Romulo Messias <romulomessias@me.com>
2019-09-23 19:42:27 -0300

    added breakpoints and global styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './styles/global.scss';
 import App from './App';
 import { register } from './ServiceWorker';
+
 
 ReactDOM.render(
     <App />,

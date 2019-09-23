c3fa8bd42c5a5e540a4d2bc2701a61345eb64a5d
Romulo Messias <romulomessias@me.com>
2019-09-23 19:43:30 -0300

    updated App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { configure } from 'mobx';
+import { Router, Link } from '@reach/router';
 import { StoreProvider } from './store/StoreProvider';
+import Layout from './components/common/Layout';
+import FactsPage from './pages/FactsPage';
+import Loading from './components/icons/Loading';
+
+const Home = lazy(() => import('./pages/Home'));
+const NotFound = lazy(() => import('./pages/NotFound'));
 
 configure({ enforceActions: 'observed' });
 
 const App: React.FC = () => (
     <StoreProvider>
-        <section>Hello World!</section>
+        <Layout>
+            <Suspense fallback={<Loading />}>
+                <Router>
+                    <Home path="/" />
+                    <FactsPage path="fact/:category" />
+                    <NotFound default />
+                </Router>
+            </Suspense>
+        </Layout>
     </StoreProvider>
 );
 
